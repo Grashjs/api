a21bac7a53827fe4379611bc64ba0d08a4f8aed2
ambou-97 <ambroise.tine@gmail.com>
2022-11-09 14:18:52 +0100

    chore: update

--- src/test/java/com/grash/ApiApplicationTests.java
+++ src/test/java/com/grash/ApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.grash;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "[spring.main.web-application-type=reactive]")
+@SpringBootTest
 class ApiApplicationTests {
 
 	@Test
