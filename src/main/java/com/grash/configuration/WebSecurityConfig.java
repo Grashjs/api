5dbc7f5d3e77b4088db3d677e7f86a4dd9a131e1
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-12-20 13:12:49 +0100

    chore: wso2 init

--- src/main/java/com/grash/configuration/WebSecurityConfig.java
+++ src/main/java/com/grash/configuration/WebSecurityConfig.java
@@ -1,20 +1,23 @@
 package com.grash.configuration;
 
-import com.grash.security.JwtTokenFilterConfigurer;
 import com.grash.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import java.net.URI;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +26,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    @Value("${client.post-logout-uri}")
+    private String postLogoutUri;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,16 +38,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
 
         // No session will be created or used by spring security
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         // Entry points
         http.authorizeRequests()//
-                .antMatchers("/auth/login")
+                .antMatchers("/auth/login").permitAll()
+                .antMatchers("/auth2/login")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .oauth2Login().loginPage("/auth/login");
+                .oauth2Login()
+                .loginPage("/auth2/login")
+                .defaultSuccessUrl("/auth2/login-success", true)
+                .failureUrl("/auth2/login-failure")
+                .and()
+                .logout().logoutUrl("/logout")
+                .logoutSuccessHandler(oidcLogoutSuccessHandler());
 //                .antMatchers("/").permitAll()
 //                .antMatchers("auth/login").permitAll()
 //                .antMatchers("/checklists").permitAll()
@@ -59,10 +73,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .oauth2Login().loginPage("/auth/login");
 
         // If a user try to access a resource without having enough permissions
-        http.exceptionHandling().accessDeniedPage("/auth/login");
+        http.exceptionHandling().accessDeniedPage("/auth2/login");
 
         // Apply JWT
-        http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
+        // http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
         http.cors();
 
         // Optional, if you want to test the API from a browser
@@ -97,4 +111,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+    /**
+     * Handles OIDC Logout redirection.
+     *
+     * @return LogoutSuccessHandler
+     */
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+
+        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler = new OidcClientInitiatedLogoutSuccessHandler(
+                this.clientRegistrationRepository);
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri(URI.create(postLogoutUri));
+        return oidcLogoutSuccessHandler;
+    }
 }
---
+++ src/main/java/com/grash/controller/Auth2Controller.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package com.grash.controller;
+
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ResolvableType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Controller
+@RequestMapping("/auth2")
+@Api(tags = "auth2")
+@RequiredArgsConstructor
+public class Auth2Controller {
+
+    private String userName;
+    private DefaultOidcUser user;
+
+    private final OAuth2AuthorizedClientService authorizedClientService;
+
+    private static String authorizationRequestBaseUri = "oauth2/authorization";
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    /**
+     * To customize the default login page to a different login page with "/oauth-login" redirection.
+     *
+     * @param model Model
+     * @return login page
+     */
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+
+        Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+
+        Iterable<ClientRegistration> clientRegistrations = null;
+        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository).as(Iterable.class);
+        if (type != ResolvableType.NONE &&
+                ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
+            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+        }
+
+        clientRegistrations.forEach(registration -> oauth2AuthenticationUrls.put(registration.getClientName(),
+                "/" + authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+
+        return "login";
+    }
+
+    /**
+     * Redirects to this method once the user successfully authenticated and this method will redirect to index page.
+     *
+     * @param model          Model.
+     * @param authentication Authentication.
+     * @return Index page.
+     */
+    @GetMapping("/login-success")
+    public String currentUserName(Model model, Authentication authentication) {
+
+        user = (DefaultOidcUser) authentication.getPrincipal();
+        if (user != null) {
+            userName = user.getName();
+        }
+        model.addAttribute("userName", userName);
+        getTokenDetails(model, authentication);
+        return "index";
+    }
+
+    /**
+     * Handles the redirection to /userinfo endpoint and get the user information from authentication object. This
+     * method will display the id-token and user information in the UI.
+     *
+     * @param authentication Authentication
+     * @param model          Model.
+     * @return userinfo html page.
+     */
+    @GetMapping("/userinfo")
+    public String getUser(Authentication authentication, Model model) {
+
+        model.addAttribute("userName", userName);
+        model.addAttribute("idtoken", user.getClaims());
+        return "userinfo";
+    }
+
+    private void getTokenDetails(Model model, Authentication authentication) {
+
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        String clientRegistrationId = oauthToken.getAuthorizedClientRegistrationId();
+        OAuth2AuthorizedClient client =
+                authorizedClientService.loadAuthorizedClient(clientRegistrationId, oauthToken.getName());
+
+        if (client != null && client.getAccessToken() != null) {
+            String accessToken = client.getAccessToken().getTokenValue();
+            Set<String> scope = client.getAccessToken().getScopes();
+            String tokenType = client.getAccessToken().getTokenType().getValue();
+            String accessTokenExp = client.getAccessToken().getExpiresAt().toString();
+            model.addAttribute("accessToken", accessToken);
+            model.addAttribute("tokenType", tokenType);
+            model.addAttribute("accessTokenExp", accessTokenExp);
+            model.addAttribute("scope", scope);
+
+        }
+
+        if (client != null && client.getRefreshToken() != null) {
+            String refreshToken = client.getRefreshToken().getTokenValue();
+            model.addAttribute("refreshToken", refreshToken);
+
+        }
+    }
+}
